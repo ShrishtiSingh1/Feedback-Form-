3b21d0d8318fb16d8457943d02c5a696e67d7ec3
ShrishtiSingh1 <144584216+ShrishtiSingh1@users.noreply.github.com>
2025-02-19 10:22:04 +0530

    settings.py

---
+++ myfeedbackform/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myfdbk',  
+        'USER': 'postgres',  
+        'PASSWORD': 'postgres', 
+        'HOST': 'localhost',  
+        'PORT': '5437',  
+    }
+}
